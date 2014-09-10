e0e189486e85ac6569bffb9ac00edb716aebfca2
Brad Sawicki <git@bradsawicki.com>
2014-09-10 18:03:21 -0500

    Dash: Recent Stats / Quick Links

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,32 +1,69 @@
 
 <div class="row">
 
+  <div class="col-1-2 block block-links">
+        
+    <div class="block-content">
+    
+      <a href="" class="link link-1">
+        <div class="icon"><span class="icon-download"></span></div>
+        Download Transactions
+      </a>
+
+      <a href="" class="link link-1">
+        <div class="icon"><span class="icon-add"></span></div>
+        Add / Edit Fundraiser Pages
+      </a>
+
+      <a href="" class="link link-1">
+        <div class="icon"><span class="icon-add"></span></div>
+        Add / Edit Event Pages
+      </a>
+
+      <a href="" class="link link-1">
+        <div class="icon"><span class="icon-view"></span></div>
+        View Event Registrations
+      </a>
+    
+    </div>
+    
+  </div>
+
   <div class="col-1-2 block block-stats">
     
     <header class="block-header">
       <h2 class="block-title">Recent Stats</h2>
+      
+      <div class="form-select">
+        <select>
+          <option>Past Week</option>
+          <option>Past Month</option>
+          <option>Past 6 Months</option>
+          <option>Past Year</option>
+        </select>
+      </div>
     </header>
     
     <div class="block-content">
     
-      <div class="stat-1">
-        <div class="number">1,000</div>
+      <div class="stat stat-1">
+        <div class="number">1,267</div>
         <div class="label">Donors</div>
       </div>
 
-      <div class="stat-2">
-        <div class="number">1,000</div>
-        <div class="label">Donors</div>
+      <div class="stat stat-2">
+        <div class="number"><sup>$</sup>482,239</div>
+        <div class="label">Raised</div>
       </div>
 
-      <div class="stat-3">
-        <div class="number">1,000</div>
-        <div class="label">Donors</div>
+      <div class="stat stat-3">
+        <div class="number">27</div>
+        <div class="label">New Members</div>
       </div>
 
-      <div class="stat-4">
-        <div class="number">1,000</div>
-        <div class="label">Donors</div>
+      <div class="stat stat-4">
+        <div class="number">189</div>
+        <div class="label">Event Registrations</div>
       </div>
     
     </div>
