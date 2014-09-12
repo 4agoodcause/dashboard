1b67cf2bbf4620fb7389c529ccd5eb5fa594e75a
Brad Sawicki <git@bradsawicki.com>
2014-09-12 15:20:03 -0500

    Add search to Edit

--- edit.php
+++ edit.php
@@ -11,7 +11,7 @@
         <a href="./data.php" class="back-link">Back to List <span class="icon-arrow-blue-right"></span></a>
       </div>
 
-      <?php // include $_SERVER['DOCUMENT_ROOT'].'/views/search.php'; ?>
+      <?php include $_SERVER['DOCUMENT_ROOT'].'/views/search.php'; ?>
 
       <?php include $_SERVER['DOCUMENT_ROOT'].'/views/data-edit.php'; ?>
     
