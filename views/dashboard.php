acb837ac0f1a1ebb7a4cf885753c40bfe284fb2f
Brad Sawicki <git@bradsawicki.com>
2014-09-15 18:25:36 -0500

    Post Blocks style

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,33 +1,27 @@
-
 <div class="row">
 
   <div class="col-1-2 block block-links">
         
     <div class="block-content">
-    
       <a href="" class="link link-1">
         <div class="icon"><span class="icon-download"></span></div>
         Download Transactions
       </a>
-
       <a href="" class="link link-1">
         <div class="icon"><span class="icon-add"></span></div>
         Add / Edit Fundraiser Pages
       </a>
-
       <a href="" class="link link-1">
         <div class="icon"><span class="icon-add"></span></div>
         Add / Edit Event Pages
       </a>
-
       <a href="" class="link link-1">
         <div class="icon"><span class="icon-view"></span></div>
         View Event Registrations
       </a>
-    
     </div>
     
-  </div>
+  </div><!-- block-links -->
 
   <div class="col-1-2 block block-stats">
     
@@ -45,29 +39,72 @@
     </header>
     
     <div class="block-content">
-    
       <div class="stat stat-1">
         <div class="number">1,267</div>
         <div class="label">Donors</div>
       </div>
-
       <div class="stat stat-2">
         <div class="number"><sup>$</sup>482,239</div>
         <div class="label">Raised</div>
       </div>
-
       <div class="stat stat-3">
         <div class="number">27</div>
         <div class="label">New Members</div>
       </div>
-
       <div class="stat stat-4">
         <div class="number">189</div>
         <div class="label">Event Registrations</div>
       </div>
-    
     </div>
     
-  </div>
+  </div><!-- block-stats -->
+
+  <div class="col-1-2 block block-posts">
+    
+    <header class="block-header">
+      <h2 class="block-title">4aGoodCause News</h2>
+      <a href="" class="aux-link">View All <span class="icon-arrow-blue-right"></span></a>
+    </header>
+    
+    <div class="block-content">
+      <article class="post">
+        <p class="post-date">Aug 28, 2014</p>
+        <h3 class="post-title"><a href="">How to encourage mobile donations from your direct mail fundraising</a></h3>
+      </article>
+      <article class="post">
+        <p class="post-date">Aug 28, 2014</p>
+        <h3 class="post-title"><a href="">How to encourage mobile donations from your direct mail fundraising</a></h3>
+      </article>
+      <article class="post">
+        <p class="post-date">Aug 28, 2014</p>
+        <h3 class="post-title"><a href="">How to encourage mobile donations from your direct mail fundraising</a></h3>
+      </article>
+    </div>
+    
+  </div><!-- block-news -->
+
+  <div class="col-1-2 block block-posts">
+    
+    <header class="block-header">
+      <h2 class="block-title">4aGoodCause News</h2>
+      <a href="" class="aux-link">Follow Us <span class="icon-arrow-blue-right"></span></a>
+    </header>
+    
+    <div class="block-content">
+      <article class="post">
+        <p class="post-date">55 M</p>
+        <h3 class="post-title">Top 4 Reasons People Unsubscribe & How to Keep Them: <a href="">http://ow.ly/z6zuD</a></h3>
+      </article>
+      <article class="post">
+        <p class="post-date">Aug 11</p>
+        <h3 class="post-title">Are You Making These 9 Unforgivable Mistakes On Your About Page? <a href="">http://ow.ly/z6zuD</a></h3>
+      </article>
+      <article class="post">
+        <p class="post-date">Aug 8</p>
+        <h3 class="post-title">No reason why you can't have fun while #fundraising. Check out the human foosball game. <a href="">http://ow.ly/z6zuD</a></h3>
+      </article>
+    </div>
+    
+  </div><!-- block-tweets -->
 
 </div>
