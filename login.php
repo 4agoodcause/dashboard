69682835f0288bdc1c02793a10f38a65bdfb31fa
Brad Sawicki <git@bradsawicki.com>
2014-10-02 14:34:21 -0500

    Login form

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>4aGoodCause | Admin Login</title>
+  <link href="/assets/css/application.css" rel="stylesheet" media="all">
+  <script type="text/javascript" src="//use.typekit.net/eaq8brz.js"></script>
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+  <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body>
+
+  <?php include $_SERVER['DOCUMENT_ROOT'].'/views/login-form.php'; ?>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="/assets/js/scripts.js"></script>
+</body>
+</html>
---
+++ views/login-form.php
@@ -0,0 +1,18 @@
+
+<form class="login-form">
+  
+  <div class="logo"></div>
+
+  <div class="form-item">
+    <input type="email" name="email" placeholder="Email Address">
+  </div>
+
+  <div class="form-item">
+    <input type="password" name="password" placeholder="Password">
+  </div>
+  
+  <button class="btn">Log In</button>
+  
+  <p><a href="" class="forgot">Forgot Password?</a></p>
+
+</form>
